7965ce7771d8022517aec751f325d9ed403e85b0
Unknown <lukaszbro@hotmail.com>
2019-01-15 20:38:33 -0800

    update

--- FakeFiniteRing.cpp
+++ FakeFiniteRing.cpp
@@ -4,86 +4,91 @@
 
 #ifndef FAKEFINITERINGs_FAKEFINITERING_H
 #define FAKEFINITERINGs_FAKEFINITERING_H
+#import "iostream"
+#include "FakeFiniteRing.h"
+#include <iostream>
 
 
-class FakeFiniteRing {
-public:
+//Returns the order of the FakeFiniteRing
+const int FakeFiniteRing::getOrder() {
+    return order;
+}
 
-    //Returns the order of the FakeFiniteRing
-    int getOrder() {
-        return order;
-    }
+//returns the value of the FakeFiniteRing
+const int FakeFiniteRing::getValue() {
+    return value;
+}
 
-    //returns the value of the FakeFiniteRing
-    int getValue() {
-        return value;
-    }
+//Constructor for a FakeFiniteRing that takes in two integers for value and order
+FakeFiniteRing::FakeFiniteRing(int val, int ord) {
+    value = val % ord;
+    order = ord;
+}
 
-    //Constructor for a FakeFiniteRing that takes in two integers for value and order
-    FakeFiniteRing(int val, int ord) {
-        value = val % ord;
-        order = ord;
-    }
+//Overloaded + function creating a new FakeFiniteRing adding the two sides together
+FakeFiniteRing FakeFiniteRing::operator+(const FakeFiniteRing& left, const FakeFiniteRing& right)  {
+    if (left.getOrder() == right.getOrder()) {
+        return FakeFiniteRing((left.getValue() + right.getValue()) % left.getOrder(), left.getOrder());
+    } else throw std::invalid_argument("bad order");
+}
 
-    //Overloaded + function creating a new FakeFiniteRing adding the two sides together
-    FakeFiniteRing operator+(const FakeFiniteRing &left, const FakeFiniteRing &right) {
-        if (left.getOrder() == right.getOrder()) {
-            return FakeFiniteRing((left.getValue() + right.getValue()) % left.getOrder(), left.getOrder());
-        } else throw exception("error");
-    }
+//Overloaded - function creating a new FakeFiniteRing subtracting the right from the left side
+FakeFiniteRing FakeFiniteRing::operator-(const FakeFiniteRing& left, const FakeFiniteRing& right) const {
+    if (left.getOrder() == right.getOrder()) {
+        return FakeFiniteRing((left.getValue() - right.getValue()) % left.getOrder(), left.getOrder());
+    } else throw std::invalid_argument("bad order");
+}
 
-    //Overloaded - function creating a new FakeFiniteRing subtracting the right from the left side
-    FakeFiniteRing operator-(const FakeFiniteRing &left, const FakeFiniteRing &right) {
-        if (left.getOrder() == right.getOrder()) {
-            return FakeFiniteRing((left.getValue() - right.getValue()) % left.getOrder(), left.getOrder());
-        } else throw exception("error");
-    }
+//overloaded * function creating a new FakeFiniteRing by multiplying the rings together
+FakeFiniteRing FakeFiniteRing::operator*(const FakeFiniteRing* left, const FakeFiniteRing& right) const {
+    if (left.getOrder() == right.getOrder()) {
+        return FakeFiniteRing((left.getValue() * right.getValue()) % left.getOrder(), left.getOrder());
+    } else throw std::invalid_argument("bad order");
+}
 
-    //overloaded * function creating a new FakeFiniteRing by multiplying the rings together
-    FakeFiniteRing operator*(const FakeFiniteRing &left, const FakeFiniteRing &right) {
-        if (left.getOrder() == right.getOrder()) {
-            return FakeFiniteRing((left.getValue() * right.getValue()) % left.getOrder(), left.getOrder());
-        } else throw exception("error");
-    }
+//overloaded == function that will return whether two FakeFiniteRings are congruent
+bool operator==(const FakeFiniteRing& left, const FakeFiniteRing& right)  {
+    if (left.getOrder() == right.getOrder() && left.getValue() = right.getValue()) {
+        return true;
+    } else return false;
+}
 
-    //overloaded == function that will return whether two FakeFiniteRings are congruent
-    bool operator==(const FakeFiniteRing &left, const FakeFiniteRing &right) {
-        if (left.getOrder() == right.getOrder() && left.getValue() = right.getValue()) {
-            return true;
-        } else return false;
-    }
+//overloaded != function that will return true if the two FakeFiniteRings are not congruent
+bool operator!=(const FakeFiniteRing &left, const FakeFiniteRing &right) const {
+    if (left.getOrder() != right.getOrder() || left.getValue() != right.getValue()) {
+        return true;
+    } else return false;
+}
 
-    //overloaded != function that will return true if the two FakeFiniteRings are not congruent
-    bool operator!=(const FakeFiniteRing &left, const FakeFiniteRing &right) {
-        if (left.getOrder() != right.getOrder() || left.getValue() != right.getValue()) {
-            return true;
-        } else return false;
-    }
+//overloaded += function that will create a new FakeFiniteRing by adding to the previous FakeFiniteRing
+FakeFiniteRing operator+=(const FakeFiniteRing &left, const FakeFiniteRing &right) {
+    return left + right;
+}
 
-    //overloaded += function that will create a new FakeFiniteRing by adding to the previous FakeFiniteRing
-    FakeFiniteRing operator+=(const FakeFiniteRing &left, const FakeFiniteRing &right) {
-        return left + right;
-    }
+//overloaded -= function that will create a new FakeFiniteRing by subtracting the right from the left FakeFiniteRing
+FakeFiniteRing operator-=(const FakeFiniteRing &left, const FakeFiniteRing &right) {
+    return left - right;
+}
 
-    //overloaded -= function that will create a new FakeFiniteRing by subtracting the right from the left FakeFiniteRing
-    FakeFiniteRing operator-=(const FakeFiniteRing& left, const FakeFiniteRing& right) {
-        return left - right;
-    }
+//overloaded *= function that will create a new FakeFiniteRing by multiplying the new FakeFiniteRing by the old one
+FakeFiniteRing operator*=(const FakeFiniteRing &left, const FakeFiniteRing &right) {
+    return left * right;
+}
 
-    //overloaded *= function that will create a new FakeFiniteRing by multiplying the new FakeFiniteRing by the old one
-    FakeFiniteRing operator*=(const FakeFiniteRing& left, const FakeFiniteRing& right) {
-        return left * right;
-    }
-    //overloaded = function that will create a new FakeFiniteRing with a shortcut instead of calling a constructor
-    FakeFiniteRing& operator= (const FakeFiniteRing &ring) {
-        return FakeFiniteRing( &ring.getValue(), &ring.getOrder() )
-    }
-    //overloaded >> function that will input the FakeFiniteRings
-    istream& operator >> (istream input, FakeFiniteRing& ring);
-    //overloaded << function that will output the FakeFiniteRings to a readable format
-    ostream& operator << (ostream& output const FakeFiniteRing& ring);
-};
+//overloaded = function that will create a new FakeFiniteRing with a shortcut instead of calling a constructor
+FakeFiniteRing &operator=(const FakeFiniteRing &ring) {
+    return FakeFiniteRing(&ring.getValue(), &ring.getOrder())
+}
 
+//overloaded >> function that will input the FakeFiniteRings
+istream &operator>>(istream &input, FakeFiniteRing ring) {
+
+}
+
+//overloaded << function that will output the FakeFiniteRings to a readable format
+ostream &operator<<(ostream &output const FakeFiniteRing ring) {
+
+}
 
 
 #endif //FAKEFINITERINGs_FAKEFINITERING_H
--- FakeFiniteRing.h
+++ FakeFiniteRing.h
@@ -2,50 +2,67 @@
 // Created by Luke on 1/14/2019.
 //
 
-#ifndef FAKEFINITERINGS_FAKEFINITERING_H
-#define FAKEFINITERINGS_FAKEFINITERING_H
-
-
+#ifndef FAKEFINITERING_H
+#define FAKEFINITERING_H
+#include <iostream>
 
 
 class FakeFiniteRing {
 public:
     //the value of the FakeFiniteRing
-    int value;
+    int value = 0;
     //the order of the FakeFiniteRing
-    int order;
+    int order = 0;
+
     //returns the order of the FakeFiniteRing
-    int getOrder();
+    const int getOrder();
+
     //returns the value of the FakeFiniteRing
-    int getValue();
+    const int getValue();
+
+    void setOrder();
+
+    void setValue();
+
     //Constructor for the FakeFiniteRing, Takes 2 ints for value and order
     FakeFiniteRing(int val, int ord);
-    bool compareTo(FakeFiniteRing ring2);
+
+    friend bool compareTo(FakeFiniteRing ring2);
+
     //Overloaded + function creating a new FakeFiniteRing adding the two sides together
-    FakeFiniteRing operator+(const FakeFiniteRing& left, const FakeFiniteRing& right);
+    friend FakeFiniteRing operator+(const FakeFiniteRing &left, const FakeFiniteRing &right);
+
     //Overloaded - function creating a new FakeFiniteRing subtracting the right from the left side
-    FakeFiniteRing operator-(const FakeFiniteRing& left, const FakeFiniteRing& right);
+    friend FakeFiniteRing operator-(const FakeFiniteRing &left, const FakeFiniteRing &right);
+
     //overloaded * function creating a new FakeFiniteRing by multiplying the rings together
-    FakeFiniteRing operator*(const FakeFiniteRing& left, const FakeFiniteRing& right);
+    friend FakeFiniteRing operator*(const FakeFiniteRing &left, const FakeFiniteRing &right);
+
     //overloaded == function that will return whether two FakeFiniteRings are congruent
-    bool operator==(const FakeFiniteRing& left, const FakeFiniteRing& right);
+    friend bool operator==(const FakeFiniteRing &left, const FakeFiniteRing &right);
+
     //overloaded != function that will return true if the two FakeFiniteRings are not congruent
-    bool operator!=(const FakeFiniteRing& left, const FakeFiniteRing& right);
+    friend bool operator!=(const FakeFiniteRing &left, const FakeFiniteRing &right);
+
     //overloaded += function that will create a new FakeFiniteRing by adding to the previous FakeFiniteRing
-    FakeFiniteRing operator+=(const FakeFiniteRing& left, const FakeFiniteRing& right);
+    friend FakeFiniteRing operator+=(const FakeFiniteRing &left, const FakeFiniteRing &right);
+
     //overloaded -= function that will create a new FakeFiniteRing by subtracting the right from the left FakeFiniteRing
-    FakeFiniteRing operator-=(const FakeFiniteRing& left, const FakeFiniteRing& right);
+    friend FakeFiniteRing operator-=(const FakeFiniteRing &left, const FakeFiniteRing &right);
+
     //overloaded *= function that will create a new FakeFiniteRing by multiplying the new FakeFiniteRing by the old one
-    FakeFiniteRing operator*=(const FakeFiniteRing& left, const FakeFiniteRing& right);
+    friend FakeFiniteRing operator*=(const FakeFiniteRing &left, const FakeFiniteRing &right);
+
     //overloaded = function that will create a new FakeFiniteRing with a shortcut instead of calling a constructor
-    FakeFiniteRing& operator= (const FakeFiniteRing &ring) = delete;
+    friend FakeFiniteRing &operator=(const FakeFiniteRing &ring) = delete;
+
     //overloaded >> function that will input the FakeFiniteRings
-    istream& operator >> (istream input, FakeFiniteRing& ring);
+    friend istream &operator>>(istream &input, FakeFiniteRing &ring);
+
     //overloaded << function that will output the FakeFiniteRings to a readable format
-    ostream& operator << (ostream& output const FakeFiniteRing& ring);
+    friend ostream &operator<<(ostream &output const FakeFiniteRing &ring);
 
 };
 
 
-
-#endif //FAKEFINITERINGS_FAKEFINITERING_H
+#endif //FAKEFINITERING_H
